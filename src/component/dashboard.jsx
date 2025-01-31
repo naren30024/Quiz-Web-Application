ba375fa79341c317f34572aea5bd5cd232972714
naren30024 <126703711+naren30024@users.noreply.github.com>
2025-01-31 14:48:03 +0530

    optimized multiple functions to a single function

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -14,117 +14,13 @@ export default function DashBoard( add,setAdd){
     let [attempt,setAttempt]= useState(0);
     let sub;
     //var attempt=0;
-    const handlesubos = () => {
+    const handleSubject = () => {
         
-        setAttempt(attempt+1);
-        setSubj("OS")
-        setQui(false)
-        
-    }
-    const handlesubds = () => {
-        setAttempt(attempt+1);
-        setSubj("DS")
-        setQui(false)
-        
-    }
-    const handlesubpps = () => {
-        setSubj("PPS")
         setAttempt(attempt+1);
         setQui(false)
         
     }
-    const handlesubml = () => {
-        setSubj("ML")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubai = () => {
-        setSubj("AI")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubcn = () => {
-        setSubj("CN")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubdms = () => {
-        setSubj("DMS")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubcd = () => {
-        setSubj("CD")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubcoa = () => {
-        setSubj("COA")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesuboop = () => {
-        setSubj("OOP")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubdbms = () => {
-        setSubj("DBMS")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubcg = () => {
-        setSubj("CG")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubdaa = () => {
-        setSubj("DAA")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubwt = () => {
-        setSubj("WT")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubflat = () => {
-        setSubj("FLAT")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubse = () => {
-        setSubj("SE")
-        setAttempt(attempt+1);
-        setQui(false)
-    }
-        
-    const handlesubda = () => {
-        setSubj("DA")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-    const handlesubacns = () => {
-        setSubj("CNS")
-        setAttempt(attempt+1);
-        setQui(false)
-        
-    }
-
-
+    
     return(
         <>
         <div className="fixed-top"><ResponsiveAppBar/></div>
@@ -135,94 +31,94 @@ export default function DashBoard( add,setAdd){
                         
                         <div className="col-sm-12 col-lg-3 p-5 m-3 bg-primary text-white subject python"  >
                             <Link to="" style={{ textDecoration: 'none' }}>
-                              <a onClick={handlesubpps} className="text-white text-decoration-none">Programming for problem solving</a>
+                              <a onClick={() => {handleSubject && setSubj("pps")}} className="text-white text-decoration-none">Programming for problem solving</a>
                             </Link>
                         </div>
                             
                     
                         <div className="col-sm-12 col-xl-3 p-5 m-3 bg-secondary text-white subject dms" >
                             <Link to="" style={{ textDecoration: 'none' }}>
-                              <a onClick={handlesubdms} className="text-white text-decoration-none">Discete Mathematical Structures</a>
+                              <a onClick={() => {handleSubject && setSubj("dms")}} className="text-white text-decoration-none">Discete Mathematical Structures</a>
                             </Link>
                         </div>
 
 
                         <div className="col-sm-12 col-xl-3 p-5 m-3 bg-success text-white subject cd" >
                             <Link to="" style={{ textDecoration: 'none' }}>  
-                              <a href="#" onClick={handlesubcd} className="text-white text-decoration-none">Compiler Design</a>
+                              <a href="#" onClick={() => {handleSubject && setSubj("cd")}} className="text-white text-decoration-none">Compiler Design</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-info text-white subject ds" onClick={handlesubds} >
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-info text-white subject ds" onClick={() => {handleSubject && setSubj("ds")}} >
                             <Link to="" style={{ textDecoration: 'none'} }>
                               <a href="#" className="text-white text-decoration-none">Data Structures</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-danger text-white subject coa" onClick={handlesubcoa}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-danger text-white subject coa" onClick={() => {handleSubject && setSubj("coa")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                               <a href="#" className="text-white text-decoration-none">Computer Organization & Architecture</a>
                               </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject os" value="Os" onClick={handlesubos}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject os" value="Os" onClick={() => {handleSubject && setSubj("os")}}>
                             <Link to="" style={{ textDecoration: 'none' }} >
                               <a href="#" className="text-white text-decoration-none">Operating System</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject oop" onClick={handlesuboop}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject oop" onClick={() => {handleSubject && setSubj("oop")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#" className="text-white text-decoration-none">Object Oriented Programming</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject dbms" onClick={handlesubdbms}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject dbms" onClick={() => {handleSubject && setSubj("dms")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                               <a href="#" className="text-white text-decoration-none">Database Management System</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cg" onClick={handlesubcg}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cg" onClick={() => {handleSubject && setSubj("cg")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Computer Graphics</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject daa" onClick={handlesubdaa}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject daa" onClick={() => {handleSubject && setSubj("daa")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Design And Analysis of Algorithms</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cn" onClick={handlesubcn}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cn" onClick={() => {handleSubject && setSubj("cn")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#" className="text-white text-decoration-none">Computer Network</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject wt" onClick={handlesubwt}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject wt" onClick={() => {handleSubject && setSubj("wt")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                               <a href="#" className="text-white text-decoration-none">Web Technologies</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject flat" onClick={handlesubflat}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject flat" onClick={() => {handleSubject && setSubj("flat")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Formal Languages & Automata Theory</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject ml" onClick={handlesubml}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject ml" onClick={() => {handleSubject && setSubj("ml")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Machine Learning</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject se" onClick={handlesubse}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject se" onClick={() => {handleSubject && setSubj("se")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#" className="text-white text-decoration-none">Software Engineering</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject da" onClick={handlesubda}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject da" onClick={() => {handleSubject && setSubj("da")}}>
                             <Link  style={{ textDecoration: 'none' }}>
                               <a href="#" className="text-white text-decoration-none">Data Analytics</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject ai" onClick={handlesubai}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject ai" onClick={() => {handleSubject && setSubj("ai")}}>
                             <Link to="" style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Artifical Intelligence</a>
                             </Link>
                         </div>
-                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cns" onClick={handlesubacns}>
+                        <div className="col-sm-12 col-xl-3 p-5 m-3 bg-dark text-white subject cns" onClick={() => {handleSubject && setSubj("cns")}}>
                             <Link  style={{ textDecoration: 'none' }}>
                                <a href="#php" className="text-white text-decoration-none">Cryptography And Network Security</a>
                             </Link>
